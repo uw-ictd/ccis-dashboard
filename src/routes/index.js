83f40b44db35517dd0bfbbba4df8568c4b3342ca
Amolo <railamolo@gmail.com>
2022-11-17 16:09:25 +0300

    submit forms to Airtable

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ const { getAuth, loginAuth } = require('../util/auth');
 const passport = require('passport');
 const getIndexData = require('../controller/getIndexData');
 const { URL_PREFIX } = require('../config/routingConstants');
+const {fetch} = require('cross-fetch');
+
+
+const token = "patlpOIg99GVB6Tsq.b4c842a0ff138d5791fdbc497365b0875cffb0cab2cab15592417d34dc1813f0";
 
 function makeRouter(db) {
     const router = express.Router();
@@ -28,9 +32,17 @@ function makeRouter(db) {
         successRedirect: URL_PREFIX
     }));
 
-    router.post('/request-account', passport.authenticate('local', {
-        successRedirect: URL_PREFIX
-    }));
+    router.post('/request-account', async (req, res) => {
+        console.log(req.body);
+        const response = await fetch('https://api.airtable.com/v0/appRmEYChnaez5sIQ/Requests',{
+            headers:{"Content-Type":"application/json", "Authorization": `Bearer ${token}`},
+            body: JSON.stringify({records:[{fields: {"Names": req.body.names, "Email": req.body.email, "Message": req.body.message}}]}),
+            method:'POST'
+        });
+        console.log((await response.json()));
+        res.render('register');
+        return
+    });
 
     return router;
 }
